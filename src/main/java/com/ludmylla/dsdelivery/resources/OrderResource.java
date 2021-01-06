f3b3dc6dfdcf9d8098a224a5b105ed6ede5c9816
Ludmylla Arielly <ludmay4@gmail.com>
2021-01-06 08:34:06 -0300

    POST /orders

--- src/main/java/com/ludmylla/dsdelivery/resources/OrderResource.java
+++ src/main/java/com/ludmylla/dsdelivery/resources/OrderResource.java
@@ -1,12 +1,16 @@
 package com.ludmylla.dsdelivery.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.ludmylla.dsdelivery.dto.OrderDTO;
 import com.ludmylla.dsdelivery.service.OrderService;
@@ -22,6 +26,15 @@ public class OrderResource {
 	public ResponseEntity<List<OrderDTO>> findAll(){
 		List<OrderDTO> list = orderService.findAll();
 		return ResponseEntity.ok(list);
+	}
+	
+	@PostMapping
+	public ResponseEntity<OrderDTO> insert(@RequestBody OrderDTO orderDTO){
+		orderDTO = orderService.insert(orderDTO);
+		URI uri = ServletUriComponentsBuilder
+				.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(orderDTO.getId()).toUri();
+		return ResponseEntity.created(uri).body(orderDTO);
 	}
 
 }
--- src/main/java/com/ludmylla/dsdelivery/service/OrderService.java
+++ src/main/java/com/ludmylla/dsdelivery/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.ludmylla.dsdelivery.service;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -8,8 +9,12 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.ludmylla.dsdelivery.dto.OrderDTO;
+import com.ludmylla.dsdelivery.dto.ProductDTO;
 import com.ludmylla.dsdelivery.entities.Order;
+import com.ludmylla.dsdelivery.entities.OrderStatus;
+import com.ludmylla.dsdelivery.entities.Product;
 import com.ludmylla.dsdelivery.repositories.OrderRepository;
+import com.ludmylla.dsdelivery.repositories.ProductRepository;
 
 @Service
 public class OrderService {
@@ -17,12 +22,27 @@ public class OrderService {
 	@Autowired
 	private OrderRepository orderRepository;
 	
+	@Autowired
+	private ProductRepository productRepository;
+	
 	@Transactional(readOnly = true)
 	public List<OrderDTO> findAll(){
 		List<Order> list = orderRepository.findOrderWithProducts();
 		return list.stream()
 				.map(x -> new OrderDTO(x))
 				.collect(Collectors.toList());
+	}
+	
+	@Transactional
+	public OrderDTO insert(OrderDTO dto){
+		Order order = new Order(null, dto.getAddress(), dto.getLatitude(), dto.getLongitude(), 
+				Instant.now(),OrderStatus.PENDING);
+		for(ProductDTO p: dto.getProducts()) {
+			Product product = productRepository.getOne(p.getId());
+			order.getProducts().add(product);
+		}
+		order = orderRepository.save(order);
+		return new OrderDTO(order);
 	}
 
 }
